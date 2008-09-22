36fe7f447b7f0bf1ecaa52518609575cd4b25c42
Ben Scofield <git@turrean.com>
2008-09-22 06:43:25 -0400

    add stubs for message, user, and tag list tests

---
+++ test/test_message_list.rb
@@ -0,0 +1,5 @@
+require "#{File.dirname(__FILE__)}/test_helper"
+
+class MessageListTest < Test::Unit::TestCase
+
+end
---
+++ test/test_user_list.rb
@@ -0,0 +1,5 @@
+require "#{File.dirname(__FILE__)}/test_helper"
+
+class UserListTest < Test::Unit::TestCase
+
+end
