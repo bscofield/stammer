418e1ff08693156ae0a950935b22ee3ddc446736
Ben Scofield <git@turrean.com>
2008-10-04 09:11:41 -0400

    add initial test for messagelist

--- test/test_message_list.rb
+++ test/test_message_list.rb
@@ -1,5 +1,8 @@
 require "#{File.dirname(__FILE__)}/test_helper"
 
 class MessageListTest < Test::Unit::TestCase
-
+  def test_message_list_is_array
+    m = Stammer::MessageList.new([])
+    assert m.is_a?(Array)
+  end
 end
