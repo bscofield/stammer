9b5eb4b6e24fa4e3d592f85fbdfc06ac9e0684d2
Ben Scofield <git@turrean.com>
2008-10-04 09:31:46 -0400

    lowercase require (for RCR, potentially); rename variable to @message for intent

--- test/test_message.rb
+++ test/test_message.rb
@@ -1,9 +1,9 @@
 require "#{File.dirname(__FILE__)}/test_helper"
-require 'Time'
+require 'time'
 
 class MessageTest < Test::Unit::TestCase
   def setup
-    @raw = {
+    @message = {
       'id' => 1,
       'sender-id' => 1,
       'sender-type' => 'user',
@@ -15,41 +15,41 @@ class MessageTest < Test::Unit::TestCase
   end
   
   def test_message_module_adds_plain_method
-    assert !@raw.respond_to?(:plain)
+    assert !@message.respond_to?(:plain)
     setup_message
-    assert @raw.respond_to?(:plain)
-    assert_equal 'Hello world!', @raw.plain
+    assert @message.respond_to?(:plain)
+    assert_equal 'Hello world!', @message.plain
   end
 
   def test_message_module_adds_sender_id_method
-    assert !@raw.respond_to?(:sender_id)
+    assert !@message.respond_to?(:sender_id)
     setup_message
-    assert @raw.respond_to?(:sender_id)
-    assert_equal 1, @raw.sender_id
+    assert @message.respond_to?(:sender_id)
+    assert_equal 1, @message.sender_id
   end
 
   def test_message_module_adds_sender_type_method
-    assert !@raw.respond_to?(:sender_type)
+    assert !@message.respond_to?(:sender_type)
     setup_message
-    assert @raw.respond_to?(:sender_type)
-    assert_equal 'user', @raw.sender_type
+    assert @message.respond_to?(:sender_type)
+    assert_equal 'user', @message.sender_type
   end
 
   def test_message_module_adds_created_method
-    assert !@raw.respond_to?(:created)
+    assert !@message.respond_to?(:created)
     setup_message
-    assert @raw.respond_to?(:created)
-    assert_equal Time.parse(@raw['created_at']), @raw.created
+    assert @message.respond_to?(:created)
+    assert_equal Time.parse(@message['created_at']), @message.created
   end
 
   def test_message_module_overrides_id_method
     setup_message
-    assert @raw.respond_to?(:id)
-    assert_equal 1, @raw.id
+    assert @message.respond_to?(:id)
+    assert_equal 1, @message.id
   end
   
   private
   def setup_message
-    @raw.extend(Stammer::Message)
+    @message.extend(Stammer::Message)
   end
 end
