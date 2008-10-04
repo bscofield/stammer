748e8fafc1f9966441b4ae0c9fb8c745f9727960
Ben Scofield <git@turrean.com>
2008-10-04 09:12:31 -0400

    add initial tests for taglist and userlist

--- test/test_tag_list.rb
+++ test/test_tag_list.rb
@@ -1,5 +1,9 @@
 require "#{File.dirname(__FILE__)}/test_helper"
 
 class TagListTest < Test::Unit::TestCase
+  def test_tag_list_is_array
+    m = Stammer::TagList.new([])
+    assert m.is_a?(Array)
+  end
 
 end
--- test/test_user_list.rb
+++ test/test_user_list.rb
@@ -1,5 +1,9 @@
 require "#{File.dirname(__FILE__)}/test_helper"
 
 class UserListTest < Test::Unit::TestCase
+  def test_user_list_is_array
+    m = Stammer::UserList.new([])
+    assert m.is_a?(Array)
+  end
 
 end
